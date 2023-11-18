711eba6f7078d439c9bd810dc82b50adfdef919f
Jeff <j.edjekoomhene@gmail.com>
2023-11-18 09:53:23 +0100

    setup init file

---
+++ wishlist/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    return app
