39f6a14ae2001c7917dbd2a4a93904b7e9511a91
Jeff <j.edjekoomhene@gmail.com>
2023-12-03 13:57:42 +0100

    add validatioon for sign up

--- wishlist/auth.py
+++ wishlist/auth.py
@@ -15,4 +15,19 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+            
     return render_template("sign_up.html")
