3bb3b56862ceaf4eae702f5e9f36e52403b86681
Jeff <j.edjekoomhene@gmail.com>
2023-12-03 13:55:17 +0100

    add login page

--- wishlist/auth.py
+++ wishlist/auth.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
+
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
