cbb86b7fcd499296441e6db9d3144b87aa12e4a0
Jeff <j.edjekoomhene@gmail.com>
2023-12-04 18:59:58 +0100

    create db and user model

--- wishlist/__init__.py
+++ wishlist/__init__.py
@@ -1,13 +1,19 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
 load_dotenv()
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
